2111d31d172cb22d6fd003d63c34ec23e99de472
Dinesh Pallagani <pallagani.dinesh@gmail.com>
2025-07-13 15:34:28 +0530

    My First commit

---
+++ src/test/java/com/facbook/test/FacebookTest.java
@@ -0,0 +1,19 @@
+package com.facbook.test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class FacebookTest {
+	@Test
+	public void launchFacebook() {
+	    WebDriver driver = new ChromeDriver();
+	    driver.manage().window().maximize(); // ✅ Maximizes the browser window
+
+	    driver.get("https://www.facebook.com");
+	    System.out.println("Title of the page is: " + driver.getTitle());
+
+	    driver.quit();
+	}
+
+}
